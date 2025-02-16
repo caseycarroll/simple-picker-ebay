240e74ec683317e283dc8b92d63429123cd752f7
Casey Carroll <aiyakc@gmail.com>
2025-02-16 09:53:51 -0800

    groups and tabs

--- stories/picker-host.stories.js
+++ stories/picker-host.stories.js
@@ -92,6 +92,110 @@ export const Groups = {
         }
     }
 }
+export const GroupsWithTabs = {
+    args: {
+        aspects: {
+            categories: [
+                {
+                    id: "men",
+                    title: "Men",
+                    groupings: [
+                        {
+                            title: "Standard",
+                            values: [
+                                {
+                                    title: "9",
+                                    id: "mens-standard-9"
+                                },
+                                {
+                                    title: "10",
+                                    id: "mens-standard-10"
+                                },
+                                {
+                                    title: "11",
+                                    id: "mens-standard-11"
+                                },
+                                {
+                                    title: "12",
+                                    id: "mens-standard-12"
+                                },
+                            ]
+                        },
+                        {
+                            title: "Wide",
+                            values: [
+                                {
+                                    title: "9",
+                                    id: "mens-wide-9"
+                                },
+                                {
+                                    title: "10",
+                                    id: "mens-wide-10"
+                                },
+                                {
+                                    title: "11",
+                                    id: "mens-wide-11"
+                                },
+                                {
+                                    title: "12",
+                                    id: "mens-wide-12"
+                                },
+                            ]
+                        },
+                    ]
+                },
+                {
+                    id: "women",
+                    title: "Women",
+                    groupings: [
+                        {
+                            title: "Standard",
+                            values: [
+                                {
+                                    title: "9",
+                                    id: "womens-standard-9"
+                                },
+                                {
+                                    title: "10",
+                                    id: "womens-standard-10"
+                                },
+                                {
+                                    title: "11",
+                                    id: "womens-standard-11"
+                                },
+                                {
+                                    title: "12",
+                                    id: "womens-standard-12"
+                                },
+                            ]
+                        },
+                        {
+                            title: "Wide",
+                            values: [
+                                {
+                                    title: "9",
+                                    id: "womens-wide-9"
+                                },
+                                {
+                                    title: "10",
+                                    id: "womens-wide-10"
+                                },
+                                {
+                                    title: "11",
+                                    id: "womens-wide-11"
+                                },
+                                {
+                                    title: "12",
+                                    id: "womens-wide-12"
+                                },
+                            ]
+                        },
+                    ]
+                },
+            ]
+        }
+    }
+}
 
 export const Tabs = {
     args: {
