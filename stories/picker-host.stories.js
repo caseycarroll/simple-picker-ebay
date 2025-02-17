b0496b0819e386a6184029e56ab24940eeb68c9e
Casey Carroll <aiyakc@gmail.com>
2025-02-16 21:52:25 -0800

    navigation host

--- stories/picker-host.stories.js
+++ stories/picker-host.stories.js
@@ -92,7 +92,57 @@ export const Groups = {
         }
     }
 }
-export const GroupsWithTabs = {
+
+export const Tabs = {
+    args: {
+        aspects: {
+            categories: [
+                {
+                    id: "men",
+                    title: "Men",
+                    values: [
+                        {
+                            title: "9",
+                            id: "mens-9"
+                        },
+                        {
+                            title: "10",
+                            id: "mens-10"
+                        },
+                        {
+                            title: "11",
+                            id: "mens-11"
+                        },
+                        {
+                            title: "12",
+                            id: "mens-12"
+                        },
+                    ]
+                },
+                {
+                    id: "women",
+                    title: "Women",
+                    values: [
+                        {
+                            title: "8",
+                            id: "womens-8"
+                        },
+                        {
+                            title: "9",
+                            id: "womens-9"
+                        },
+                        {
+                            title: "10",
+                            id: "womens-10"
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+}
+
+export const TabsWithGroups = {
     args: {
         aspects: {
             categories: [
@@ -197,51 +247,117 @@ export const GroupsWithTabs = {
     }
 }
 
-export const Tabs = {
+export const TabsWithRowsWithGroups = {
     args: {
         aspects: {
             categories: [
                 {
                     id: "men",
                     title: "Men",
-                    values: [
+                    subcategories: [
                         {
-                            title: "9",
-                            id: "mens-9"
-                        },
-                        {
-                            title: "10",
-                            id: "mens-10"
-                        },
-                        {
-                            title: "11",
-                            id: "mens-11"
-                        },
-                        {
-                            title: "12",
-                            id: "mens-12"
-                        },
-                    ]
+                            title: "Mens Pants",
+                            groupings: [
+                                {
+                                    title: "Standard",
+                                    values: [
+                                        {
+                                            title: "9",
+                                            id: "mens-standard-9"
+                                        },
+                                        {
+                                            title: "10",
+                                            id: "mens-standard-10"
+                                        },
+                                        {
+                                            title: "11",
+                                            id: "mens-standard-11"
+                                        },
+                                        {
+                                            title: "12",
+                                            id: "mens-standard-12"
+                                        },
+                                    ]
+                                },
+                                {
+                                    title: "Wide",
+                                    values: [
+                                        {
+                                            title: "9",
+                                            id: "mens-wide-9"
+                                        },
+                                        {
+                                            title: "10",
+                                            id: "mens-wide-10"
+                                        },
+                                        {
+                                            title: "11",
+                                            id: "mens-wide-11"
+                                        },
+                                        {
+                                            title: "12",
+                                            id: "mens-wide-12"
+                                        },
+                                    ]
+                                },
+                            ]
+                        }
+                    ] 
                 },
                 {
                     id: "women",
                     title: "Women",
-                    values: [
+                    subcategories: [
                         {
-                            title: "8",
-                            id: "womens-8"
-                        },
-                        {
-                            title: "9",
-                            id: "womens-9"
-                        },
-                        {
-                            title: "10",
-                            id: "womens-10"
+                            title: "Womens Pants",
+                            groupings: [
+                                {
+                                    title: "Standard",
+                                    values: [
+                                        {
+                                            title: "9",
+                                            id: "womens-standard-9"
+                                        },
+                                        {
+                                            title: "10",
+                                            id: "womens-standard-10"
+                                        },
+                                        {
+                                            title: "11",
+                                            id: "womens-standard-11"
+                                        },
+                                        {
+                                            title: "12",
+                                            id: "womens-standard-12"
+                                        },
+                                    ]
+                                },
+                                {
+                                    title: "Wide",
+                                    values: [
+                                        {
+                                            title: "9",
+                                            id: "womens-wide-9"
+                                        },
+                                        {
+                                            title: "10",
+                                            id: "womens-wide-10"
+                                        },
+                                        {
+                                            title: "11",
+                                            id: "womens-wide-11"
+                                        },
+                                        {
+                                            title: "12",
+                                            id: "womens-wide-12"
+                                        },
+                                    ]
+                                },
+                            ]
                         }
-                    ]
+                    ],
                 }
             ]
-        }
+        },
     }
 }
